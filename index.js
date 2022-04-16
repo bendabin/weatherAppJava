3965f355208c9c073b93146ceb6ab0f2eb0026fa
bendabin <dabin100@gmail.com>
2022-04-16 16:14:50 +0900

    Uploaded files

---
+++ index.js
@@ -0,0 +1,54 @@
+
+
+for (let i = 0; i < document.getElementsByClassName("btn").length; ++i) {
+
+    document.getElementsByClassName("btn")[i].addEventListener("click", function () {
+        alert("It codes beautifully");
+
+    });
+}
+
+
+
+
+// document.getElementsByClassName("btn-chg")[0].addEventListener("click", handleTheClick);
+
+
+// function handleTheClick(){
+
+//     alert("You clicked me!"); 
+// }
+
+
+
+
+// document.getElementById("btn-chg").addEventListener("click", function(){
+
+//     alert("Hello you clicked me!");
+// });
+
+// let noBtns = document.getElementsByClassName("btn-chg").length;
+
+// for  (let i = 0; i < noBtns; i++){
+
+// document.getElementsByClassName("btn-chg")[i].addEventListener("click", function(){
+
+//     alert("Hello give me a good job " + i);
+
+// });
+
+// }
+
+
+// document.getElementById("btn-chg").addEventListener("click", handleClick);
+
+
+
+
+// function handleClick(){
+
+//     alert("Hello you clicked me!");
+
+// }
+
+
