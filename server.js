0638cc8f616e83adf3a2ddd0b5b4ad1ac0b3e0b1
bendabin <dabin100@gmail.com>
2022-04-25 22:18:20 +0900

    configured the server and ran some tests

---
+++ server.js
@@ -0,0 +1,24 @@
+/*
+    This is the server that will be used to host our website and to communicate with the API's    
+*/ 
+
+const express = require("express");                //This creates a new package called express 
+const app = express();                             //Create a new const; a function that represents the express module 
+
+app.use(express.urlencoded({extended: true}));
+
+
+//Handling 'GET' requests from the web browser
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+
+
+//This will be the channel or port that we host our server on 
+//Host the server on Localhost: 3000
+app.listen(3000, function(){
+    console.log("Server is running on port 3000")
+});                                 
